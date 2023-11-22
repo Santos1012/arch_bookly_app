8a41235fa6cfb3dbbe27d30fe0912f1c8803fb29
Omar Ehab Santawy <111521164+Santos1012@users.noreply.github.com>
2023-11-22 23:28:03 +0200

    add book entity

---
+++ lib/features/home/domain/entities/book_entities.dart
@@ -0,0 +1,17 @@
+class BookEntities {
+  final String imageUrl;
+  final String title;
+  final String subTitle;
+  final num price;
+  final num rating;
+  final int ratingCount;
+
+  BookEntities({
+    required this.imageUrl,
+    required this.title,
+    required this.subTitle,
+    required this.price,
+    required this.rating,
+    required this.ratingCount,
+  });
+}
