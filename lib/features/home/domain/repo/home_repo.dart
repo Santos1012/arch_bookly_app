8bb3c1bee699c34b4e125994167f159002f44317
Omar Ehab Santawy <111521164+Santos1012@users.noreply.github.com>
2023-11-22 23:40:44 +0200

    add Either return to HomeRepo class

--- lib/features/home/domain/repo/home_repo.dart
+++ lib/features/home/domain/repo/home_repo.dart
@@ -1,8 +1,9 @@
-
-
+import 'package:bookly_app/core/errors/failure.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entities.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntities>> fetchFeatureBooks();
-  Future<List<BookEntities>> fetchNewestBooks();
+  Future<Either<Failure,List<BookEntities>>> fetchFeatureBooks();
+  Future<Either<Failure,List<BookEntities>>> fetchNewestBooks();
+  
 }
