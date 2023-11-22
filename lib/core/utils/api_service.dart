a0a4bdbff603006befaf62c4f617a884869f25d0
Omar Ehab Santawy <111521164+Santos1012@users.noreply.github.com>
2023-11-23 01:17:11 +0200

    create ApiService class

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio _dio;
+  final String _baseUrl = "https://www.googleapis.com/books/v1/";
+
+  ApiService({required Dio dio}) : _dio = dio;
+  Future<Map<String, dynamic>> grt({required String endPoints}) async {
+    var response = await _dio.get("$_baseUrl$endPoints");
+    return response.data;
+  }
+}
