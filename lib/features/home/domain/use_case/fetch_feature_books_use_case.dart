89630c008211d32efd7144be87e5cdaad42e36b7
Omar Ehab Santawy <111521164+Santos1012@users.noreply.github.com>
2023-11-23 00:06:21 +0200

    refactor & Generic FetchFeatureBooksUseCase class

--- lib/features/home/domain/use_case/fetch_feature_books_use_case.dart
+++ lib/features/home/domain/use_case/fetch_feature_books_use_case.dart
@@ -1,13 +1,16 @@
 import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/use_cases/use_case.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entities.dart';
 import 'package:bookly_app/features/home/domain/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeatureBooksUseCase {
+class FetchFeatureBooksUseCase extends UseCase<List<BookEntities>, NoParam> {
   final HomeRepo homeRepo;
 
   FetchFeatureBooksUseCase({required this.homeRepo});
-  Future<Either<Failure, List<BookEntities>>> call() {
-    return homeRepo.fetchFeatureBooks();
+
+  @override
+  Future<Either<Failure, List<BookEntities>>> call([NoParam? param]) async {
+    return await homeRepo.fetchFeatureBooks();
   }
 }
