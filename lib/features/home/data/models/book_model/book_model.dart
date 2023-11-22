ff19db9d9a93d44e22abd570bd8974eebe93817d
Omar Ehab Santawy <111521164+Santos1012@users.noreply.github.com>
2023-11-23 00:53:44 +0200

    make BookModel class extends BookEntities class

--- lib/features/home/data/models/book_model/book_model.dart
+++ lib/features/home/data/models/book_model/book_model.dart
@@ -1,13 +1,13 @@
-import 'package:equatable/equatable.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entities.dart';
 
 import 'access_info.dart';
 import 'layer_info.dart';
 import 'sale_info.dart';
 import 'volume_info.dart';
 
-class BookModel extends Equatable {
+class BookModel extends BookEntities {
   final String? kind;
-  final String? id;
+  final String id;
   final String? etag;
   final String? selfLink;
   final VolumeInfo volumeInfo;
@@ -15,20 +15,29 @@ class BookModel extends Equatable {
   final SaleInfo? saleInfo;
   final AccessInfo? accessInfo;
 
-  const BookModel({
+  BookModel({
     this.kind,
-    this.id,
+    required this.id,
     this.etag,
     this.selfLink,
     required this.volumeInfo,
     this.layerInfo,
     this.saleInfo,
     this.accessInfo,
-  });
+  }) : super(
+          bookId: id,
+          imageUrl: volumeInfo.imageLinks?.thumbnail??"",
+          title: volumeInfo.title!,
+          authorName: volumeInfo.authors?.first??"No Author",
+          description: volumeInfo.description,
+          price: 00.00,
+          rating: volumeInfo.averageRating,
+          ratingCount: volumeInfo.ratingsCount,
+        );
 
   factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
         kind: json['kind'] as String?,
-        id: json['id'] as String?,
+        id: json['id'] as String,
         etag: json['etag'] as String?,
         selfLink: json['selfLink'] as String?,
         volumeInfo:
@@ -54,18 +63,4 @@ class BookModel extends Equatable {
         'saleInfo': saleInfo?.toJson(),
         'accessInfo': accessInfo?.toJson(),
       };
-
-  @override
-  List<Object?> get props {
-    return [
-      kind,
-      id,
-      etag,
-      selfLink,
-      volumeInfo,
-      layerInfo,
-      saleInfo,
-      accessInfo,
-    ];
-  }
 }
--- lib/features/home/domain/entities/book_entities.dart
+++ lib/features/home/domain/entities/book_entities.dart
@@ -1,17 +1,19 @@
 class BookEntities {
-  final int bookId;
-  final String imageUrl;
+  final String bookId;
+  final String? imageUrl;
   final String title;
-  final String subTitle;
-  final num price;
-  final num rating;
-  final int ratingCount;
+  final String? authorName;
+  final String? description;
+  final num? price;
+  final num? rating;
+  final int? ratingCount;
 
   BookEntities({
     required this.bookId,
     required this.imageUrl,
     required this.title,
-    required this.subTitle,
+    required this.authorName,
+    required this.description,
     required this.price,
     required this.rating,
     required this.ratingCount,
