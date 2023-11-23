a7219483d652b038d8b436a115af062f2c925b7d
Omar Ehab Santawy <111521164+Santos1012@users.noreply.github.com>
2023-11-23 06:53:59 +0200

    Implement home remote fetch featured books

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -5,7 +5,7 @@ class ApiService {
   final String _baseUrl = "https://www.googleapis.com/books/v1/";
 
   ApiService({required Dio dio}) : _dio = dio;
-  Future<Map<String, dynamic>> grt({required String endPoints}) async {
+  Future<Map<String, dynamic>> get({required String endPoints}) async {
     var response = await _dio.get("$_baseUrl$endPoints");
     return response.data;
   }
--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entities.dart';
 
 abstract class HomeRemoteDataSource {
@@ -6,14 +8,29 @@ abstract class HomeRemoteDataSource {
 }
 
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImpl({required this.apiService});
   @override
-  Future<List<BookEntities>> fetchFeatureBooks() {
-    throw UnimplementedError();
+  Future<List<BookEntities>> fetchFeatureBooks() async {
+    Map<String, dynamic> data = await apiService.get(
+      endPoints:
+          "volumes?maxResults=40&Filtering=free-ebooks&Sorting=relevance: flutter&q=subject:programming&orderBy=newest",
+    );
+    List<BookEntities> books = parseJsonToBookEntitiesMethod(data);
+    return books;
   }
 
   @override
   Future<List<BookEntities>> fetchNewestBooks() {
     throw UnimplementedError();
   }
-  
+}
+
+List<BookEntities> parseJsonToBookEntitiesMethod(Map<String, dynamic> data) {
+  List<BookEntities> books = [];
+  for (var items in data["items"]) {
+    books.add(BookModel.fromJson(items));
+  }
+  return books;
 }
